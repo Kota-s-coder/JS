486d8bdc4bdf827200f007f1fbc4e31cf23a303b
kotasawamoto <kota.sawamoto.engineer@gmail.com>
2025-05-11 16:07:20 +0900

    前半セクション完了

---
+++ app.js
@@ -0,0 +1,22 @@
+// 変数宣言はconst, let
+// とりあえず変数名はconstを使え
+
+alert("Hello World!");
+
+// 配列
+const fistPokemons = ["ニャオハ", "ホゲータ", "クワッス"];
+
+console.log(fistPokemons);
+console.log(fistPokemons[1]);
+
+// forループ文
+// for(初期値; 条件式; 増減){}
+
+const questions = [
+    "No.1",
+    "No.2",
+    "No.3"
+]
+for(let index = 0; index < questions.length; index++){
+    console.log(questions[index]);
+}
